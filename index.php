76c6bdac61b97a1b5df46f3f6a99ccda081e3f43
Austin Norman <asimnoman2000@gmail.com>
2024-12-20 02:25:50 +0500

    commit

--- index.php
+++ index.php
@@ -240,18 +240,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		}
     </style>
 	
-	<!-- Google Tag Manager -->
+<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MRQQ73S5');</script>
+})(window,document,'script','dataLayer','GTM-N2X7FPHV');</script>
 <!-- End Google Tag Manager -->
 </head>
 <body>
 	
-	<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRQQ73S5"
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2X7FPHV"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
